0240f61f8f8c1bdf6f564dedf3e8bc140daac4a5
susangautam <off.susangautam@gmail.com>
2025-01-14 15:30:47 +0545

    push

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-6">
       <div className="container mx-auto text-center">
-        <p>&copy; 2025 ShopMate. All rights reserved.</p>
+        <p>&copy; 2025 Hamro Pasal. All rights reserved.</p>
       </div>
     </footer>
   );
